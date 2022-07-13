9f98749495913647ae36a36560af60d35e5b848c
sachin magar <gitsachin123@gmail.com>
2022-07-13 10:06:38 +0545

    discounticon

---
+++ ResturantSystem/Controllers/Admin/Discount/DiscountController.cs
@@ -0,0 +1,19 @@
+﻿using ResturantSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResturantSystem.Controllers.Admin.Discount
+{
+    public class DiscountController : Controller
+    {
+        ResturantsystemEntities db;
+        // GET: Discount
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
